bb7411d712f735d08819717af9ea7aa564c58152
andresousajr <andresousajr@outlook.com>
2018-07-12 18:52:27 -0300

    commit Andre

--- src/main/java/com/zup/Zup.java
+++ src/main/java/com/zup/Zup.java
@@ -14,6 +14,7 @@ public class Zup {
 		persons.add(new Person(2, "Jose", 20, generatePassword("Jose")));
 		persons.add(new Person(3, "Maria", 23, generatePassword("Maria")));
 		persons.add(new Person(4, "Alfredo", 18, generatePassword("Alfredo")));
+		persons.add(new Person(5, "Rivs", 31, generatePassword("Rivs")));
 
 		// print all persons
 		for (Person person : persons) {
