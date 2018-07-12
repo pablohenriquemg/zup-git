adae46235486fd363dd9567bb1c5d665416a90a3
Carlos  Silva <carlos.silva@zup.com.br>
2018-07-12 18:49:28 -0300

    commit Carlinhao

--- src/main/java/com/zup/Zup.java
+++ src/main/java/com/zup/Zup.java
@@ -14,6 +14,7 @@ public class Zup {
 		persons.add(new Person(2, "Jose", 20, generatePassword("Jose")));
 		persons.add(new Person(3, "Maria", 23, generatePassword("Maria")));
 		persons.add(new Person(4, "Alfredo", 18, generatePassword("Alfredo")));
+		persons.add(new Person(4, "Carlinhao", 22, generatePassword("Carlinhao")));
 
 		// print all persons
 		for (Person person : persons) {
